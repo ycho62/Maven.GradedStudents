796ec5308121a625dc4a8c455ef499f49bcc16cd
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-08 17:47:11 -0500

    changes made to classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,45 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Classroom {
+    private Student[] students;
+
+    public Classroom(int maxNumberOfStudents) {
+        students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] student) {
+        students = student;
+    }
+
+    public Classroom(){
+        this(30);
+    }
+
+    public Student[] getStudents() {
+        return getStudents();
+    }
+
+    public Double getAverageExamScore() {
+        double averageScore = 0.0;
+        double sum = 0.0;
+        for(int i = 0; i < students.length; i++) {
+            sum += students[i].getAverageExamScore();
+
+        }
+        return sum / students.length;
+    }
+
+    public void addStudent(Student student) {
+        Arrays.fill(students, student);
+    }
+    //for (int i =0; i< student.length; i++) {
+//    student[i] = student);
+    public void removeStudent(String firstName, String lastName ) {
+
+    }
+
 }
+
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,77 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Student {
-}
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student (String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>(Arrays.asList(testScores));
+    }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+        return lastName;
+        }
+
+
+        public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        }
+
+        public void setLastName(String lastName) {
+        this.lastName = lastName;
+        }
+
+        public Integer getNumberOfExamsTaken(){
+        return examScores.size();
+        }
+
+        public String getExamScores() {
+        String scores ="";
+        for (int i = 0; i < getNumberOfExamsTaken(); i++){
+            scores+= (i+1) + "Exam " + "score was"+ examScores.get(i) +"\n";
+        }
+        return scores;
+        }
+
+        public void addExamScore(double examScore) {
+        examScores.add(examScore);
+
+        }
+
+        public void setExamScores(int examNumber, double newScore) {
+        examScores.set(examNumber-1, newScore);
+        }
+
+        public Double getAverageExamScore() {
+        double sum = 0;
+        double output;
+        for (int i = 0; i< getNumberOfExamsTaken(); i++) {
+            sum += examScores.get(i);
+
+
+        }return output = sum/getNumberOfExamsTaken();
+
+        }
+
+        public String toString() {
+        String student = "Student name: " + getFirstName() + getLastName() +"\n" +
+                "Test score: " + getExamScores() + "\n" + "Average test scores: " +
+                getAverageExamScore();
+
+
+            return student;
+    }
+    }
+
